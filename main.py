62cd12ce6fa6cb6647d9de0fff9cd133565c3dda
josephineeho <josephineeho8@gmail.com>
2025-08-22 15:54:00 +0100

    added main.py file

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+app.route('/')
+def home():
+    return "<h1>Welcome to the Home Page!</h1>"
+
+# An enpoint to return an object of schoos
+@app.route('/schools', methods=['GET'])
+def get_schools():
+    schools = [
+        {'name': 'ABC High School', 'location': 'New York'},
+        {'name': 'XYZ Academy', 'location': 'Los Angeles'},
+        {'name': '123 Institute', 'location': 'Chicago'}
+    ]
+    return jsonify(schools)
